bdfce9930ad94f518ef8b588c3f58cfd3db0560b
Olena <lenanavorska@gmail.com>
2024-09-16 23:40:16 +0200

    aaaa2

--- ft_printf.c/ft_printf.c
+++ ft_printf.c/ft_printf.c
@@ -6,59 +6,57 @@
 /*   By: olena <olena@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 12:42:13 by olena             #+#    #+#             */
-/*   Updated: 2024/09/16 21:06:45 by olena            ###   ########.fr       */
+/*   Updated: 2024/09/16 23:34:18 by olena            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf_parse(const char *form, int i, va_list arg)
-{
-	int		c;
 
-	c = 0;
-	if (form[i + 1] == 'c')
-		c += ft_printf_char(va_arg(arg, int));
-	else if (form[i + 1] == 's')
-		c += ft_printf_str(va_arg(arg, char *));
-	else if (form[i + 1] == 'd' || form[i + 1] == 'i')
-		c += ft_printf_nbr(va_arg(arg, int));
-	else if (form[i + 1] == 'u')
-		c += ft_printf_unsigned(va_arg(arg, unsigned int));
-	else if (form[i + 1] == 'x')
-		c += ft_printf_hex(va_arg(arg, unsigned int), 0);
-	else if (form[i + 1] == 'X')
-		c += ft_printf_hex(va_arg(arg, unsigned int), 1);
-	else if (form[i + 1] == 'p')
-		c += ft_printf_pointer(va_arg(arg, unsigned long long));
-	else if (form[i + 1] == '%')
-		c += ft_printf_percent();
-	return (c);
+static	int	ft_formats(va_list args, const char format)
+{
+	int	print_length;
+
+	print_length = 0;
+	if (format == 'c')
+		print_length += ft_printf_char(va_arg(args, int));
+	else if (format == 's')
+		print_length += ft_printf_str(va_arg(args, char *));
+	else if (format == 'p')
+		print_length += ft_printf_pointer(va_arg(args, unsigned long long));
+	else if (format == 'd' || format == 'i')
+		print_length += ft_printf_nbr(va_arg(args, int));
+	else if (format == 'u')
+		print_length += ft_printf_unsigned(va_arg(args, unsigned int));
+	else if (format == 'x')
+		print_length += ft_printf_hex(va_arg(args, unsigned int), 0);
+	else if (format == 'X')
+		print_length += ft_printf_hex(va_arg(args, unsigned int), 1);
+	else if (format == '%')
+		print_length += ft_printf_percent();
+	return (print_length);
 }
 
-
-int	ft_printf(const	char	*form, ...)
+int	ft_printf(const char *str, ...)
 {
 	int		i;
-	int		c;
 	va_list	args;
+	int		print_length;
 
 	i = 0;
-	c = 0;
-	va_start(args, form);
-	while(form[i])
+	print_length = 0;
+	va_start(args, str);
+	while (str[i])
 	{
-		if (form[i] == '%')
+		if (str[i] == '%')
 		{
-			c += ft_printf_parse(form, i, args);
+			print_length += ft_formats(args, str[i + 1]);
 			i++;
 		}
 		else
-		{
-			c += ft_printf_char(form[i]);
-		}
+			print_length += ft_printf_char(str[i]);
 		i++;
 	}
 	va_end(args);
-	return (c);
+	return (print_length);
 }
