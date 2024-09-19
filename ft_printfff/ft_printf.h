730588ed0a3ad397a46e7640cdc5b0a2d75c0349
Olena Tolmachova <oltolmac@c4r2p5.42vienna.com>
2024-09-19 12:38:19 +0200

    aaaaaaaaaaaaa

---
+++ ft_printfff/ft_printf.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oltolmac <oltolmac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 12:59:30 by olena             #+#    #+#             */
+/*   Updated: 2024/09/18 15:42:18 by oltolmac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# include <stdint.h>
+
+int		ft_printf(const char *form, ...);
+int		ft_printf_unsigned(unsigned int n);
+int		ft_printf_hex(unsigned int n, int x);
+int		ft_printf_nbr(int d);
+int		ft_printf_unsigned(unsigned int n);
+int		ft_printf_percent(void);
+int		ft_printf_str(char *s);
+int		ft_printf_char(int c);
+int		ft_printf_pointer(unsigned long long ptr);
+
+
+#endif
